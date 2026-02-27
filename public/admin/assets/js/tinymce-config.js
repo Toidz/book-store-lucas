ab0189e578666ea210cabb163a5a319910c1ee4d
Nguyen Dat <tymmin146@gmail.com>
2026-02-27 16:40:22 +0700

    update tiny

--- public/admin/assets/js/tinymce-config.js
+++ public/admin/assets/js/tinymce-config.js
@@ -2,6 +2,7 @@
 function initTinyMCE(id) {
   tinymce.init({
     selector: id || '[textarea-mce]',
+    apiKey: process.env.TINY_MCE,  
     plugins: 'charmap codesample emoticons help image link lists advlist media preview searchreplace table wordcount',
     toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | charmap | codesample | emoticons | image | link | numlist bullist | media | preview | searchreplace | help',
     images_upload_url:`/${pathAdmin}/upload/image`
